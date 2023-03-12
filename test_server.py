2a2d0d9ec02617fca8da76cd63d521a9c2f0c935
M Mohsin Ali <mohsin.ali@ascend.com.sa>
2023-03-12 19:17:46 +0500

    fixing base64

--- test_server.py
+++ test_server.py
@@ -4,17 +4,17 @@ import base64
 from io import BytesIO
 
 
-with open("test.jpg", "rb") as f:
+with open("n01440764_tench.jpeg", "rb") as f:
     im_b64 = base64.b64encode(f.read())
 
 
 model_inputs = {
-  "prompt": f"{im_b64}",
+  "prompt": str(im_b64),
 }
 
-api_key = "27138ec9-7d2a-4833-878d-9176d9200400"
-model_key = "a54c9c7d-f6b3-47aa-bcaf-0dac6ca5fca2"
+api_key = "27138ec9-7d2a-4833-878d-***"
+model_key = "a54c9c7d-f6b3-47aa-bcaf-***"
 
 # Run the model
-#out = banana.run(api_key, model_key, model_inputs)
-print(model_inputs)
+out = banana.run(api_key, model_key, model_inputs)
+#print(model_inputs)
